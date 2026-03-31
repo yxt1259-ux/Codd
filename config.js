3201f98bb3690055fba0d2e1ab32ce6f39be18ae
yxt1259-ux <yxt1259@gmail.com>
2026-03-31 23:36:58 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// 配置文件 - 请妥善保管此文件，不要提交到版本控制系统
+const config = {
+    qwenApiKey: 'sk-7ed0eb90d209467aa12721b522855af3',
+    amapApiKey: '你的高德地图API Key'
+};
---
+++ server.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8080
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
+    print(f"服务器运行在 http://localhost:{PORT}")
+    httpd.serve_forever()
